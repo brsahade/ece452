5516fd4fdeacce6b663a07d85209b10e01892f4b
Dave <hsungje@gmail.com>
2018-06-19 19:20:30 -0400

    layout implementation done

--- app/src/main/java/com/example/fitboyz/thirdeye/PhotoActivity.java
+++ app/src/main/java/com/example/fitboyz/thirdeye/PhotoActivity.java
@@ -2,17 +2,27 @@ package com.example.fitboyz.thirdeye;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.bumptech.glide.Glide;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.net.URI;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class PhotoActivity extends AppCompatActivity {
 
@@ -20,12 +30,40 @@ public class PhotoActivity extends AppCompatActivity {
     // this way we know we're looking at the response from our own action
     private static final int SELECT_PICTURE = 1;
 
-    private String selectedImagePath;
+    private Uri selectedImagePath;
+    private ImageView mMainImage;
+    private LinearLayout mTypeOne, mTypeTwo, mTypeThree;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
 
+        mMainImage = (ImageView)findViewById(R.id.photo_view);
+
+        mTypeOne = (LinearLayout)findViewById(R.id.image_type_one);
+        mTypeTwo = (LinearLayout)findViewById(R.id.image_type_two);
+        mTypeThree = (LinearLayout)findViewById(R.id.image_type_three);
+
+        mTypeOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                daltonization(1);
+            }
+        });
+
+        mTypeTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                daltonization(2);
+            }
+        });
+
+        mTypeThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                daltonization(3);
+            }
+        });
 
         // in onCreate or any event where your want the user to
         // select a file
@@ -37,6 +75,14 @@ public class PhotoActivity extends AppCompatActivity {
 
     }
 
+    public void daltonization(int typeId) {
+
+        Bitmap bitmap = loadBitmap(selectedImagePath);
+//        Bitmap bitmapConverted = someting(bitmap, typeId);
+        updateImage(bitmap);
+
+    }
+
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK) {
             if (requestCode == SELECT_PICTURE) {
@@ -45,45 +91,43 @@ public class PhotoActivity extends AppCompatActivity {
 //                    File file = new File(new URI(selectedImageUri.toString()));
 //                    ImageView imageView = (ImageView) findViewById(R.id.photo_view);
 //
-//                    Glide.with(this).load(file).into(imageView);
+                    selectedImagePath = selectedImageUri;
+//                    Glide.with(this).load(selectedImageUri).into(mMainImage);
+                Glide.with(this).load(R.drawable.test).into(mMainImage);
 
             }
         }
     }
-    public String getPath(Uri uri)
-    {
-        String[] projection = { MediaStore.Images.Media.DATA };
-        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
-        if (cursor == null) return null;
-        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-        cursor.moveToFirst();
-        String s = cursor.getString(column_index);
-        cursor.close();
 
-        return s;
+    private byte[] bitmapToByte(Bitmap bitmap){
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+        return byteArray;
     }
 
-//    public String getPath(Uri uri) {
-//        // just some safety built in
-//        if( uri == null ) {
-//            // TODO perform some logging or show user feedback
-//            return null;
-//        }
-//        // try to retrieve the image from the media store first
-//        // this will only work for images selected from gallery
-//        String[] projection = { MediaStore.Images.Media.DATA };
-//
-//        Cursor cursor = this.getContentResolver().query(uri, projection, null, null, null);
-//        if( cursor != null ){
-//            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-//            cursor.moveToFirst();
-//
-//            String path = cursor.getString(column_index);
-//            cursor.close();
-//
-//            return path;
-//        }
-//        // this is our fallback here
-//        return uri.getPath();
-//    }
+    public void updateImage(Bitmap b) {
+        Glide.with(this).asBitmap().load(bitmapToByte(b)).into(mMainImage); //>>not tested
+
+//        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+//        b.compress(Bitmap.CompressFormat.PNG, 100, stream);
+//        Glide.with(this)
+//                .asBitmap()
+//                .load(stream.toByteArray())
+//                .into(mMainImage);
+
+    }
+
+    public Bitmap loadBitmap(Uri url)
+    {
+        Bitmap bitmap = null;
+        try {
+            bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), url);
+            return bitmap;
+        } catch (Exception e) {
+            e.fillInStackTrace();
+        }
+        return bitmap;
+    }
+
 }
